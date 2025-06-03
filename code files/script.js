5b5c6f5ce36830b3553e1d42560a327921b984ad
Tuğçe ünlü <72610188+tugceunlu@users.noreply.github.com>
2025-06-03 23:39:45 +0300

    Delete code files directory

--- code files/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for reaching out! We will get back to you soon.');
-});
